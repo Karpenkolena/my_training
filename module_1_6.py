5a6eeba2e2f4b02a7a19abb39128444f3d53a53f
Karpenkolena <karpenkolena062@gmail.com>
2024-10-10 19:26:19 +0700

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+my_dict = {'Anna': 1997, 'Olga': 2001, 'Igor': 1999}
+print(my_dict)
+print(my_dict.get('Anna'))
+print(my_dict.get('Pavel', 'Ошибка'))
+my_dict.update({'Inna': 2000,
+            'Piter': 1998})
+a = my_dict.pop('Igor')
+print(a)
+print(my_dict)
+my_set = {3,3,5,5,5,4,'d','f','d','f'}
+print(my_set)
+e2 = (25,1.35)
+my_set.update(e2)
+print(my_set)
+
