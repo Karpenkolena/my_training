b95e14aad8f435cc224d3634179bd9968892bbd6
Karpenkolena <karpenkolena062@gmail.com>
2024-10-10 17:57:37 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var = (8,6,'d','w')
+tuple = (immutable_var)
+print(tuple)
+question = input("почему нельзя изменить значения элементов кортежа: ")
+mutable_list = [4,5,'d','w',]
+print(mutable_list)
+mutable_list[0] = 8
+print(mutable_list)
+mutable_list.append('List')
+print(mutable_list)
