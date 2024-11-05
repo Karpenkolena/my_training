81c7bacb3edea7c25e6fb61a7f6255f803d9262c
Karpenkolena <karpenkolena062@gmail.com>
2024-11-05 18:12:57 +0700

    Add files via upload

--- module_2_5.py
+++ module_2_5.py
@@ -1,15 +1,6 @@
-def get_matrix(n, m, value):
-    matrix = []
-    for i in range(n):
-        strings = []
-        matrix.append(strings)
-        for j in range(m):
-            strings.append(value)
-    return matrix
-
-result1 = get_matrix(2, 2, 10)
-result2 = get_matrix(3, 5, 42)
-result3 = get_matrix(4, 2, 13)
-print(result1)
-print(result2)
-print(result3)
+grades =[[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+midl_grades = (sum(grades[0]) / len(grades[0]), sum(grades[1]) / len(grades[1]), sum(grades[2]) / len(grades[2]),
+               sum(grades[3]) / len(grades[3]), sum(grades[4]) / len(grades[4]))
+dictionary = dict(zip(sorted(students), midl_grades))
+print(dictionary)
