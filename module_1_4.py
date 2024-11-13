634c9a04058afb3e2b0c4a93a8744dcb6c49b012
Karpenkolena <karpenkolena062@gmail.com>
2024-11-13 18:42:48 +0700

    Add files via upload

--- module_1_4.py
+++ module_1_4.py
@@ -2,8 +2,8 @@ my_string = input("Когда пойдёт дождь: ")
 print(my_string .upper())
 print(my_string .lower())
 print(my_string .replace(' ' , ''))
-print(my_string .replace('с' , ''))
-print(my_string .replace('ю' , ''))
+print(my_string .replace('к' , ''))
+print(my_string .replace('т' , ''))
 
 
 
